822280c7d0abf436cd99782c8bc7bcd9a487f4b0
drslax <anas.elouargui@gmail.com>
2020-04-06 23:36:30 +0100

    edit

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 def Scraper():
 
-    url = 'http://www.covidmaroc.ma/Pages/Accueil.aspx'
+    url = 'http://www.covidmaroc.ma/Pages/AccueilAR.aspx'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
     page = requests.get(url, headers=headers)
